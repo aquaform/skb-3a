d510ebfa01c6ae1b18555ab0f2985ad97a356c27
aquaform <aquaform@yandex.ru>
2016-11-17 23:19:13 +0500

    Прошел 30 тестов

--- src/index.js
+++ src/index.js
@@ -1,51 +1,56 @@
 import express from 'express';
 import cors from 'cors';
-import request from 'request';
 import fetch from 'isomorphic-fetch';
 
 const app = express();
 app.use(cors());
 
-let pc = {};
+var pc = {};
 
+app.get('/volumes', (req, res) => {
+	let volumes = {};
+	pc.hdd.forEach(hdd => {
+		console.log(hdd.size);
+		const currSize = volumes[hdd.volume] || 0;
+		volumes[hdd.volume] = currSize + hdd.size;
+	});
+	for(let key in volumes){
+		volumes[key] = `${volumes[key]}B`;
+	};
 
-app.get(/([\w]+)?\/?$/, (req, res) => {  
+	res.json(volumes);
+});
 
-  let parts = req.url.split('/');
-  parts = parts.slice(1);  
-  let part = pc;
+app.get(/([\w]+)?\/?$/, (req, res) => {
+	let parts = req.url.replace(/\/$/,'').split('/');
+	parts = parts.slice(1);
 
-  parts.forEach((item, i, arr) => { part = part[item] });
-  
+	let part = pc;
+	let validUrl = true;
 
-  
-   
-  res.json(part);
+	parts.forEach((item, i, arr) => {
+		if (!!part[item] || part[item]===0 || part[item] === null) {
+			part = part[item];
+		} else {
+			validUrl = false;
+		}
+	});
+
+	if (validUrl) {
+		res.json(part);
+	} else {
+		res.send (404, 'Not Found')
+	}
 });
 
 
-
-
-
-
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');  
-  const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
-
-  fetch(pcUrl)
-  	.then(async (res) => {
+	console.log('Your app listening on port 3000!');
+	const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+	fetch(pcUrl).then(async (res) => {
   		pc = await res.json();  		
   	})
   	.catch (err => {
   		console.log('Невозможно прочитать урл. Ошибка:', err);
   	});
-
-  	/*
-  	request.get(pcUrl, function (error, response, body) {    
-    	if(!error) {
-        	pc = JSON.parse(body);
-        	console.log(pc);
-    	}
-    });
-	*/
 });
